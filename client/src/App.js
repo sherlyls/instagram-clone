4ce313a0551a633c48296fce2f9150e2099b21b5
Sherly <sherlyls862@gmail.com>
2023-12-27 08:02:55 +0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import './App.css';
+// import {BrowserRouter, Route}
+
 function App() {
   return (
-    <div className="App">
-t
-    </div>
+    <Navbar />
   );
 }
 
